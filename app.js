968700539bc33b5cc7f8912a4a84ab450a3490fa
Atif Faisal <atiffaisal33@gmail.com>
2025-01-23 21:20:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//ALERTS
+
+alert ("Error!  Please enter a valid password") ;
+
+alert ("welcome to JS Land.. \n Happy coding!")
+
+console.log("Hello.. i can run JS through my web browser's console ")
+
+
+
+//VARIABLES FOR STRINGS
+
+let username;
+
+let myName = alert ("Atif faisal");
+
+let message = alert ("Hello world");
+
+
+let name = alert("Atif faisal");
+
+let age =  alert(16);
+
+let passion =  alert("Learning Modern web development");
+
+let pizza = alert ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+let email = alert("atiffaisal33@gmail.com");
+
+let book = alert("Asmarter way to learn JavaScript");
+
+let a = "Yah! i can write HTML content through JavaScript";
+document.write(a);
+
+let b = alert( "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+let c =  "\n▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ \n" ;
+
+document.write(c);
+
+
+
+
+
+// VARIABLES FOR NUMBERS
+
+
+let age1 = alert (16);
+
+let tracker = alert(" you have visited this website 14 times");
+
+let birthYear = "\n \n 2008";
+
+document.write(birthYear);
+
+let name2 = "\n \n John Doe";
+
+document.write(name2);
+
+let title = "\n \n T-shirt";
+
+document.write(title);
+
+let quantity = "\n \n John Doe orderd 5 T-shirts"
+
+document.write(quantity);
+
+
+
+
+
+
